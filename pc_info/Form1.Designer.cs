70bcea2b4d09d122cd1ad1580ec382409f8bf1dd
Цвик Владислав <vladyslav.tsvyk@nure.ua>
2022-11-23 22:01:54 +0200

    Добавьте файлы проекта.

---
+++ pc_info/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace pc_info
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Процесор",
+            "Відеокарта",
+            "Чіпсет",
+            "Батарея",
+            "Біос",
+            "Оперативна пам\'ять",
+            "Кеш",
+            "USB",
+            "Диски",
+            "Мережевий адаптер"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 39);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(776, 399);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Назва";
+            this.columnHeader1.Width = 210;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Значення";
+            this.columnHeader2.Width = 501;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "PCInfo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+    }
+}
+
---
+++ pc_info/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+
+
+namespace pc_info
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string key = string.Empty;
+            switch (comboBox1.SelectedItem.ToString())
+            {
+                case "Процесор":
+                    key = "Win32_Processor";
+                    break;
+                case "Відеокарта":
+                    key = "Win32_VideoController";
+                    break;
+                case "Чіпсет":
+                    key = "Win32_IDEController";
+                    break;
+                case "Батарея":
+                    key = "Win32_Battery";
+                    break;
+                case "Біос":
+                    key = "Win32_BIOS";
+                    break;
+                case "Оперативна пам'ять":
+                    key = "Win32_PhysicalMemory";
+                    break;
+                case "Кеш":
+                    key = "Win32_CacheMemory";
+                    break;
+                case "USB":
+                    key = "Win32_USBController";
+                    break;
+                case "Диски":
+                    key = "Win32_DiskDrive";
+                    break;
+                case "Мережевий адаптер":
+                    key = "Win32_NetworkAdapter";
+                    break;
+                default:
+                    key = "Win32_Processor";
+                    break;
+            }
+            GetHardWareInfo(key, listView1);
+        }
+
+
+        private void GetHardWareInfo(string key, ListView list)
+        {
+            list.Items.Clear();
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM " + key);
+            try
+            {
+                var items = searcher.Get();
+                if (items.Count == 0)
+                    list.Items.Add(new ListViewItem("Не вдалося отримати інформацію"));
+                else
+                    foreach (ManagementObject obj in items)
+                    {
+                        if (obj.Properties.Count == 0)
+                        {
+                            MessageBox.Show("Не вдалося отримати інформацію", "Помилка",
+                           MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            return;
+                        }
+                        ListViewGroup listViewGroup;
+                        try
+                        {
+                            listViewGroup = list.Groups.Add(obj["Name"].ToString(),
+                           obj["Name"].ToString());
+                        }
+                        catch
+                        {
+                            listViewGroup = list.Groups.Add(obj.ToString(), obj.ToString());
+                        }
+                        foreach (PropertyData data in obj.Properties)
+                        {
+                            ListViewItem item = new ListViewItem(listViewGroup);
+                            if (list.Items.Count % 2 == 0)
+                            {
+                                item.BackColor = Color.WhiteSmoke;
+                            }
+                            item.Text = data.Name;
+
+                            if (data.Value != null && !string.IsNullOrEmpty(data.Value.ToString()))
+                            {
+                                string resStr = string.Empty;
+                                switch (data.Value.GetType().ToString())
+                                {
+                                    case "System.String[]":
+                                        string[] stringData = data.Value as string[];
+                                        foreach (string s in stringData)
+                                        {
+                                            resStr += s + " ";
+                                        }
+                                        item.SubItems.Add(resStr);
+                                        break;
+                                    case "System.UInt16[]":
+                                        ushort[] ushortData = data.Value as ushort[];
+                                        foreach (ushort us in ushortData)
+                                        {
+                                            resStr += us.ToString() + " ";
+                                        }
+                                        item.SubItems.Add(resStr);
+                                        break;
+                                    default:
+                                        item.SubItems.Add(data.Value.ToString());
+                                        break;
+                                }
+                                list.Items.Add(item);
+                            }
+                        }
+                    }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Помилка", MessageBoxButtons.OK,
+                MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0;
+        }
+    }
+}
